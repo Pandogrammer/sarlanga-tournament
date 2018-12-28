d40d21129bcbbc5a81d6a84eb6cd3202dee67d14
fhermes <fhermes@asesconsulting.com>
2018-12-28 17:26:04 -0300

    Caca

--- src/main/java/farguito/sarlanga/tournament/cards/ConstantEffect.java
+++ src/main/java/farguito/sarlanga/tournament/cards/ConstantEffect.java
@@ -1,8 +1,21 @@
 package farguito.sarlanga.tournament.cards;
 
+import java.nio.file.WatchEvent.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class ConstantEffect extends Effect {
 	
 	private int duration;
+	private List<Predicate> conditions = new ArrayList<>();
+	
+	private List<Effect> effects = new ArrayList<>();	
+	private List<Modifier> modifiers = new ArrayList<>();
+	
+	public ConstantEffect(int duration) {
+		this.duration = duration;
+	}
 
 	public boolean hasExpired() {
 		return duration <= 0;
@@ -10,6 +23,18 @@ public class ConstantEffect extends Effect {
 	
 	public void removeDuration() {
 		duration--;
+	}
+	
+	public void addCondition(Predicate condition) {
+		conditions.add(condition);
+	}
+
+	public boolean hasModifiers() {
+		return !modifiers.isEmpty();
+	}
+	
+	public boolean generatesEffects() {
+		return !effects.isEmpty();
 	}
 	
 }
--- src/main/java/farguito/sarlanga/tournament/cards/actions/BattleCry.java
+++ src/main/java/farguito/sarlanga/tournament/cards/actions/BattleCry.java
@@ -1,19 +1,32 @@
 package farguito.sarlanga.tournament.cards.actions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import farguito.sarlanga.tournament.cards.Action;
+import farguito.sarlanga.tournament.cards.ConstantEffect;
 import farguito.sarlanga.tournament.cards.Effect;
+import farguito.sarlanga.tournament.cards.LastingEffect;
+import farguito.sarlanga.tournament.cards.effects.immediate.Damage;
 
 public class BattleCry extends Action {
 
-	private int fatigue = 18;
+	private int fatigue = 24;
 	private int duration = 10;
 	private double damageBonus = 1.10;
 	
 	@Override
 	public List<Effect> execute() {
-		return null;
+		
+		List<Effect> ef = new ArrayList<>();
+		this.getObjectives().stream().forEach(o -> {
+			ef.add(new ConstantEffect(duration, cooldown, new Damage(lastingDamage, o)));
+			ef.add(new Damage(damage, o));
+		});
+		
+		this.getActor().fatigate(fatigue);
+		
+		return ef;
 	}
 
 	@Override
