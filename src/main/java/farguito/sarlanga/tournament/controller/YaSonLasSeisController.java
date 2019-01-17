3e50fbf7f991935704193dae15966d36e3d85b28
fhermes <fhermes@asesconsulting.com>
2019-01-17 14:05:55 -0300

    Caquita para gon

---
+++ src/main/java/farguito/sarlanga/tournament/controller/YaSonLasSeisController.java
@@ -0,0 +1,23 @@
+package farguito.sarlanga.tournament.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ya-son-las-seis")
+public class YaSonLasSeisController {
+
+	
+	@GetMapping
+	public String sonLasSeis() {
+		LocalDateTime now = LocalDateTime.now();
+		if(now.getHour() < 18) {
+			return "No, son las "+now.getHour()+":"+String.format("%02d", now.getMinute());
+		}
+		return "SI :D";
+	}
+	
+}
